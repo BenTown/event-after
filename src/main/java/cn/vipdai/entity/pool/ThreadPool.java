2ac69069388e458ce1c5f849d30587573a8ae4fb
guandufei <guandufei@foxmail.com>
2016-11-30 09:10:31 +0800

    ThreedPool-modify

--- src/main/java/cn/vipdai/entity/pool/ThreadPool.java
+++ src/main/java/cn/vipdai/entity/pool/ThreadPool.java
@@ -4,25 +4,24 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 
 public class ThreadPool {
-    private static final int PoolSize = 100;
-
+    private static final int PoolSize = 128;
     /**
      * 活动
      */
-    ExecutorService threedPoolEvent = Executors.newCachedThreadPool();
+    private static final ExecutorService threedPoolEvent = Executors.newFixedThreadPool(PoolSize);
     /**
      * 通知（sms/email/letter）
      */
-    ExecutorService threedPoolNotice = Executors.newFixedThreadPool(PoolSize);
+    private static final ExecutorService threedPoolNotice = Executors.newFixedThreadPool(PoolSize);
 
     private ThreadPool() {
     }
 
-    public ExecutorService getThreedPoolEvent() {
+    public static ExecutorService getThreedPoolEvent() {
         return threedPoolEvent;
     }
-    
-    public ExecutorService getThreedPoolNotice() {
+
+    public static ExecutorService getThreedPoolNotice() {
         return threedPoolNotice;
     }
 }
